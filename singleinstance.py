46bd91df739becf14492d0b5b33d85dafc667974
Gyoergy Kohut <gyoergy.kohut@tu-dortmund.de>
2022-11-28 17:12:52 +0100

    Some more code comments

--- singleinstance.py
+++ singleinstance.py
@@ -1,5 +1,8 @@
 import sys, os, time
 
+# template for a script that should ensure that only one instance gets running
+# via POSIX lock on a lockfile
+
 # define a lockfile path for the script
 LOCKFILE="/tmp/singleinstance.py.lock"
 # can define a custom exit status for the case when another holds the lock
@@ -19,9 +22,9 @@ def script():
 
     import fcntl, errno
     with open(f"{LOCKFILE}", 'w+') as lockfile:
-        # acquire lock on lockfile exclusive or fail immediately
+        # acquire exclusive lock on lockfile or fail immediately (LOCK_NB)
         try:
-            fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB) # acquire lock
             lockfile.write(f"{os.getpid()} {time.time()}") # can write some info into the lockfile (pidfile)
             lockfile.flush()
             # call entry point function for the main program here
@@ -29,7 +32,9 @@ def script():
             # have to hold lock until exit
             sys.exit(ret)
         except OSError as e:
-            if e.errno in [errno.EWOULDBLOCK]: # linux: EWOULDBLOCK
+            # OSError raised and errno is set if another process already holds the lock
+            # on linux, mac == EWOULDBLOCK with LOCK_NB
+            if e.errno in [errno.EWOULDBLOCK]:
                 print(f"{__file__}: another instance is running", file=sys.stderr)
                 sys.exit(RETURNCODE_ANOTHERINSTANCE)
 
